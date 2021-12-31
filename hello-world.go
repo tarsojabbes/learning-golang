20831da74bee83d4ee2d2d8c53433183c4dd745a
Tarso Jabbes <tarsojabbes@gmail.com>
2021-12-30 22:31:35 -0300

    Reading user input

--- hello-world.go
+++ hello-world.go
@@ -1,5 +1,20 @@
 package main
 import "fmt"
 func main(){
-    fmt.Println("Hello world")
+    
+    var name string
+   
+    # Reads a line and assigns the result to name
+    fmt.Scanln(&name)
+    
+    var year_of_birth int
+
+    # Reads another line and assigns the result to year_of_birth
+    fmt.Scanln(&year_of_birth)
+
+    # Calculates the age based on the read line
+    age := 2021 - year_of_birth
+
+    fmt.Println("Hello " + name)
+    fmt.Println(age)
 }
