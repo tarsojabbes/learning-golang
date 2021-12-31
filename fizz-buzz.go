5b5e98835968f5117248aee90165c7449a808d55
Tarso Jabbes <tarsojabbes@gmail.com>
2021-12-31 10:28:48 -0300

    Basic for-loop

--- fizz-buzz.go
+++ fizz-buzz.go
@@ -4,12 +4,15 @@ import "fmt"
 
 // Training flux control
 func main() {
+
 	var n int
 	fmt.Scanln(&n)
-
 	result_fizz_buzz := fizz_buzz(n)
-
 	fmt.Println(result_fizz_buzz)
+
+	var m int
+	fmt.Scanln(&m)
+	fizz_buzz_for_loop(m)
 }
 
 func fizz_buzz(n int) string {
@@ -21,5 +24,19 @@ func fizz_buzz(n int) string {
 		return "Buzz"
 	} else {
 		return "Neither Fizz nor Buzz"
+	}
+}
+
+func fizz_buzz_for_loop(m int) {
+	for i := 0; i <= m; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz!")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz!")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz!")
+		} else {
+			fmt.Println(i)
+		}
 	}
 }
